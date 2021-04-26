2a64f4ded41376ceca61f9470591a041b1798910
matt-blodgett <35699475+matt-blodgett@users.noreply.github.com>
2021-04-26 13:49:52 -0400

    Add axios

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -2,18 +2,55 @@ import React from 'react';
 
 import Button from '@material-ui/core/Button';
 
-export default function Home () {
-  const [value, setValue] = React.useState(0);
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
-  const onValueClick = () => {
-    setValue(value + 1);
+import apiClient from '../api';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    apiVersionTitle: {
+      fontWeight: 'normal'
+    },
+    button: {
+      width: '200px',
+      marginBottom: '5px'
+    },
+    buttonContainer: {
+      marginTop: '10px',
+      display: 'inline-flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  })
+);
+
+
+export default function Home () {
+  const classes = useStyles();
+
+  const [apiVersion, setApiVersion] = React.useState<string>('');
+
+  const getApiVersion = (): void => {
+    apiClient.get('/api/version').then(response => {
+      setApiVersion(`v${response.data.version}`);
+    }).catch(error => {
+      console.log(error);
+    });
+  };
+
+  const resetApiVersion = (): void => {
+    setApiVersion('');
   };
 
   return (
-    <div>
+    <div style={{margin: '10px', fontSize: '16px'}}>
       <h1>Home Page</h1>
-      <div>Value {value}</div>
-      <Button variant="contained" color="primary" onClick={() => onValueClick()}>Increment</Button>
+      <div className={classes.apiVersionTitle}><b>API Version:</b> {apiVersion}</div>
+      <div className={classes.buttonContainer}>
+        <Button className={classes.button} variant="contained" color="primary" onClick={() => getApiVersion()}>Get Version</Button>
+        <Button className={classes.button} variant="contained" color="secondary" onClick={() => resetApiVersion()}>Reset Version</Button>
+      </div>
     </div>
   );
 }
